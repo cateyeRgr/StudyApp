574adaeff058a1bf31a3b199aebcdbfe1ce50731
cateye0434 <katharina.mller13@gmail.com>
2021-05-30 22:14:19 +0200

    FlashCards auf GitHub sichern

---
+++ FlashCards11/MainWindow.xaml.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Data.SqlClient;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Windows.Data;
+using System.Linq;
+using System.Windows.Controls;
+using System.Collections.Generic;
+
+namespace FlashCards11
+{
+    public partial class MainWindow : Window
+    {
+        //string connectionString = Connection.GetDBConnection().ToString();
+        int labelCount;
+        int countNextClicks;
+        int questionsAskedCount;
+        double correctAnswersCount;
+        string contentLbl;
+
+        private ICollectionView CollectionView;
+        private StudyAppEntities Context = new StudyAppEntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadGrid();
+            CollectionView = CollectionViewSource.GetDefaultView(Context.StudyAppItem.Local);
+
+            nextBtn.Content = "Start";
+            confirmLblQuest.Content = "";
+            subjectCB.SelectedIndex = 0;
+
+            yesRbtn.Visibility = Visibility.Hidden;
+            noRbtn.Visibility = Visibility.Hidden;
+            showBtn.IsEnabled = false;
+            yesRbtn.IsEnabled = false;
+            noRbtn.IsEnabled = false;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Context.StudyAppItem.Load();
+            CollectionView = CollectionViewSource.GetDefaultView(Context.StudyAppItem.Local);
+            ParentGrid.DataContext = CollectionView;
+
+            //populate Combo-Box
+            using (var db = new StudyAppEntities())
+            {
+
+                var queryS = from s in db.StudyAppItem
+                             //every item occurs only once
+                             group s by s.item_subject into newGroup
+                             orderby newGroup.Key ascending
+                             select newGroup;
+
+                foreach (var item in queryS)
+                {
+                    var icS = item.Key;
+                    ComboBoxItem itmS = new ComboBoxItem
+                    {
+                        Content = icS
+                    };
+
+                    subjectCB.Items.Add(itmS);
+                }
+                subjectCB.Items.Add("Neues Fach");
+            }
+        }
+
+        private async void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Test, if entry fields are not empty
+            if (queryContentTB.Text.Length > 0 && contentTB.Text.Length > 0)
+            {
+                //SqlConnection sqlCon = new SqlConnection(connectionString);
+                SqlConnection sqlCon = ServerConnection.GetDBConnection();
+                String sql = "Insert into StudyAppItem (item_name, item_content, item_subject) values ('" + queryKeywordTB.Text + "','" + contentTB.Text + "', '" + subjectCB.Text + "')";
+                SqlCommand sqlCom = new SqlCommand(sql, sqlCon);
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                String stichwortText = queryKeywordTB.Text;
+                String inhaltText = contentTB.Text;
+
+                adapter.InsertCommand = new SqlCommand(sql, sqlCon);
+                sqlCon.Open();
+
+                try
+                {
+                    adapter.InsertCommand.ExecuteNonQuery();
+                }
+                catch (Exception)
+                {
+                    confirmLblQuest.Visibility = Visibility.Visible;
+                    confirmLblQuest.Content = "Datenbank nicht erreichbar";
+                }
+
+                sqlCom.Dispose();
+                sqlCon.Close();
+                ClearTB();
+                confirmLbl.Content = "Daten wurden gespeichert";
+                await Task.Delay(3000);
+                confirmLbl.Content = "";
+
+            }
+            else
+            {
+                confirmLbl.Content = "Alle Felder ausfüllen.";
+            }
+        }
+
+        private void nextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            labelCount = 0;
+            countNextClicks++;
+
+            //reset values after each "next" click
+            nextBtn.Content = "Nächste";
+            confirmLblQuest.Content = "";
+            queryContentTB.Text = "";
+            showBtn.IsEnabled = true;
+            yesRbtn.IsChecked = false;
+            noRbtn.IsChecked = false;
+            yesRbtn.Visibility = Visibility.Hidden;
+            noRbtn.Visibility = Visibility.Hidden;
+
+            getRowCount();
+            setRandomLabels(labelCount);
+            questionsAskedCount++;
+
+            //next button doesn't work until one radio button is checked
+            if (yesRbtn.IsChecked == false && noRbtn.IsChecked == false)
+            {
+                nextBtn.IsEnabled = false;
+            }
+        }
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ClearTB();
+        }
+
+        void ClearTB()
+        {
+            //fachTB.Text = "";
+            //subjectTB.Clear();
+            //stichwortTB.Text = "";
+            queryKeywordTB.Clear();
+            //inhaltTB.Text = "";
+            contentTB.Clear();
+            confirmLbl.Content = "";
+        }
+
+        // to select random database item:
+        public int randomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        public int getRowCount()
+        {
+            //SqlConnection sqlCon = new SqlConnection(connectionString);
+            SqlConnection sqlCon = ServerConnection.GetDBConnection();
+            String sql = "Select * from StudyAppItem";
+            sqlCon.Open();
+            SqlCommand sqlCom = new SqlCommand(sql, sqlCon);
+            SqlDataReader reader = sqlCom.ExecuteReader();
+
+            try
+            {
+                while (reader.Read())
+                {
+                    labelCount++;
+                }
+                reader.Close();
+                sqlCom.Dispose();
+                sqlCon.Close();
+            }
+            catch (Exception)
+            {
+                queryContentTB.Text = "Datenbank nicht erreichbar";
+            }
+            return 0;
+        }
+
+        public void setRandomLabels(int count)
+        {
+            //SqlConnection sqlCon = new SqlConnection(connectionString);
+            SqlConnection sqlCon = ServerConnection.GetDBConnection();
+
+            //Random Number between beginning and end of table
+            int randomInt = randomNumber(0, count);
+            string randomIntString = randomInt.ToString();
+
+            String sql = "Select item_subject, item_name, item_content from StudyAppItem where item_id =" + randomIntString;
+            SqlCommand sqlCom = new SqlCommand(sql, sqlCon);
+            sqlCon.Open();
+            SqlDataReader reader = sqlCom.ExecuteReader();
+
+            try
+            {
+                while (reader.Read())
+                {
+                    querySubjectLbl.Content = reader["item_subject"].ToString();
+                    queryKeywordLbl.Content = reader["item_name"].ToString();
+                    //save content for later use when 'Lösung' is clicked
+                    contentLbl = reader["item_content"].ToString();
+                }
+                reader.Close();
+                sqlCom.Dispose();
+                sqlCon.Close();
+            }
+            catch (Exception)
+            {
+                confirmLblQuest.Content = "Datenbank nicht erreichbar";
+            }
+            return;
+        }
+
+        private void solutionBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Show radio buttons and question after first click on "next"
+            confirmLblQuest.Content = "Richtig gelöst?";
+            yesRbtn.Visibility = Visibility.Visible;
+            noRbtn.Visibility = Visibility.Visible;
+            yesRbtn.IsEnabled = true;
+            noRbtn.IsEnabled = true;
+            //pass value from Datareader
+            queryContentTB.Text = contentLbl;
+
+        }
+
+        private void yesRbtn_Click(object sender, RoutedEventArgs e)
+        {
+            //disable second radiobutton/ only one time choosing per question, enabling "next" button
+            correctAnswersCount++;
+            setPercentage();
+            yesRbtn.IsEnabled = false;
+            noRbtn.IsEnabled = false;
+            nextBtn.IsEnabled = true;
+        }
+
+        private void noRbtn_Click(object sender, RoutedEventArgs e)
+        {
+            setPercentage();
+            yesRbtn.IsEnabled = false;
+            noRbtn.IsEnabled = false;
+            nextBtn.IsEnabled = true;
+
+        }
+
+        //calculate percentage of right answers
+        public void setPercentage()
+        {
+            double correctPerc = (correctAnswersCount * 100) / questionsAskedCount;
+            pointsPercLbl.Content = string.Format("{0:0.00} %", correctPerc);
+        }
+
+        private void stopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            countNextClicks = 0;
+            questionsAskedCount = 0;
+            correctAnswersCount = 0;
+            pointsPercLbl.Content = 0;
+
+            nextBtn.Content = "Start";
+            nextBtn.IsEnabled = true;
+            confirmLblQuest.Content = "";
+            yesRbtn.Visibility = Visibility.Hidden;
+            noRbtn.Visibility = Visibility.Hidden;
+            yesRbtn.IsEnabled = false;
+            noRbtn.IsEnabled = false;
+        }
+
+        //Load knowledge table content
+        private void LoadGrid()
+        {
+            var data = from r in Context.StudyAppItem select r;
+            dgSearch.ItemsSource = data.ToList();
+        }
+
+        //private void TbFilter_TextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        //{
+        //    string filterStr = tbFilter.Text;
+        //    CollectionView.Filter = (x => ((StudyAppItem)x).item_content.Contains(filterStr));
+            
+        //}
+
+        private void TbFilter_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+            string filterStr = tbFilter.Text;
+            CollectionView.Filter = (x => ((StudyAppItem)x).item_content.Contains(filterStr));
+        }
+    }
+}
---
+++ FlashCards11/ServerConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FlashCards11
+{
+    class ServerConnection
+    {
+        public static SqlConnection GetDBConnection()
+        {
+            //Server/Instance; TableName; SQL-Login
+            //string connectionString = @"Data Source=DESKTOP-KM\SQLEXPRESS; Initial Catalog=StudyApp; Persist Security Info=True;User ID = sa; Password = sa";
+            string connectionString = @"Data Source=DESKTOP-KM\SQLEXPRESS; Initial Catalog=StudyApp; Persist Security Info=True; Integrated Security=SSPI";
+            SqlConnection con = new SqlConnection(connectionString);
+            return con;
+        }
+    }
+}
+
